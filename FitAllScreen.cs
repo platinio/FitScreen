b94a867930d5dd4bdae9fa8e254fd2d15758dd07
James Roman <platinio94@gmail.com>
2016-08-16 18:36:57 -0400

    script add

---
+++ FitAllScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//simple script for made a sprite fit all the screen
+
+public class FitAllScreen : MonoBehaviour 
+{
+	void Start()
+	{
+
+		SpriteRenderer render = GetComponent<SpriteRenderer> ();
+
+		if(render == null)
+			return;
+
+		float worldScreenHeight = Camera.main.orthographicSize * 2;
+		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+		transform.localScale = new Vector3 (worldScreenWidth / render.sprite.bounds.size.x,	worldScreenHeight / render.sprite.bounds.size.y, 1);
+	}
+}
